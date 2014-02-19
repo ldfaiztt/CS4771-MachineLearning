0d110eca8995cec5ae59e35e0d513f51750fff45
Emily Schultz <emily.schultz@newscred.com>
2014-02-18 20:11:30 -0500

    A3: Q1 SimPolyhedra implemented

--- A3/Assignment03.Problem01/SimPolyHedra.m
+++ A3/Assignment03.Problem01/SimPolyHedra.m
@@ -1,19 +1,43 @@
 function x = SimPolyHedra(N, Bounds, Polyhedra)
 % simulate N uniformly-drawn points with bounds of polyhedra
-% 1st stage of uniformly drawing > N points within box of Bounds
 
-% generate N uniformly random vectors of dim D
-points = rand(N, size(Bounds, 1));
+% % 1st stage of uniformly drawing >= N points within box of Bounds
+% % generate N uniformly random vectors of dim D
+% % I chose 2*N, because I did
+% points = rand(2*N, size(Bounds, 1));
+% 
+% % transform random numbers to within values determined by Bounds
+% for i = 1:size(Bounds,1)
+%     max = Bounds(i, 2);
+%     min = Bounds(i, 1);
+%     points(:, i) = (max - min) * points(:, i) + min;
+% end;
 
-% transform random numbers to within values determined by Bounds
-for i = 1:size(Bounds,1)
-    max = Bounds(i, 2);
-    min = Bounds(i, 1);
-    points(:, i) = (max - min) * points(:, i) + min;
+% output N points (N x D array)
+out_points = [];
+
+while(size(out_points, 1) < N)
+    % 1st stage: generate a random point
+    cur_point = rand(1, size(Bounds, 1));
+    % transform point to within Bounds
+    for i = 1 : size(Bounds, 1)
+        max = Bounds(i, 2);
+        min = Bounds(i, 1);
+        cur_point(:, i) = (max - min) * cur_point(:, i) + min;
+    end;
+
+    % 2nd stage
+    % test to see if point is inside at least one M_i
+    for p = 1:size(Polyhedra, 1)
+        % check if good, then add and break;
+        cur_M = Polyhedra{p};
+        x = [1; cur_point(:)];
+        
+        if cur_M * x >= 0
+            out_points = [out_points; cur_point];
+            break;
+        end;
+    end;
+
 end;
-
-% 2nd stage of filtering (only the first N within the polyhedra)
-
-
-
-x = points;
+x = out_points;
