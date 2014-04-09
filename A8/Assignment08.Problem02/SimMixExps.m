728ee2de43252d849976c657b5994ced506d0d67
Emily Schultz <emily.schultz@newscred.com>
2014-04-09 02:08:28 -0400

    A8: Q2 completed

--- A8/Assignment08.Problem02/SimMixExps.m
+++ A8/Assignment08.Problem02/SimMixExps.m
@@ -10,3 +10,25 @@ function [ mix_of_exps ] = SimMixExps( lambdas, proportions, N)
 %% Output:
 % mix_of_exps - a vector containing N values from the aforementioned mixture
 % 
+
+% ensure column vectors
+lambdas = lambdas(:);
+proportions = proportions(:);
+
+% count number to simulate of each lambda
+lam_counts = zeros(size(lambdas));
+% simulate N values
+for i = 1:N
+    % find which distribution based on proportions
+    lam_index = find(rand() <= cumsum(proportions),1);
+    lam_counts(lam_index) = lam_counts(lam_index) + 1;
+end;
+
+% create the empty output vector
+mix_of_exps = [];
+for i = 1:size(lam_counts,1)
+    % sample this distribution with this lambda
+    lambda = lambdas(i);
+    pd = makedist('Exponential','mu',1/lambda);
+    mix_of_exps = [mix_of_exps; random(pd, lam_counts(i),1)];
+end;
